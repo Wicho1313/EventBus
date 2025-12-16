e6c8b07ad28806282b2e93fd875bbca4cbf67eb8
Luis Enrique Rojas <LuisEnrique.Rojas@thomsonreuters.com>
2025-12-16 16:40:12 -0600

    refactor event bus class structure

--- bus/event_bus.py
+++ bus/event_bus.py
@@ -8,13 +8,23 @@ class EventBus:
         self.suscribers = {}
 
     def publish(self, event):
+        """
+        Publish an event to all subscribers.
+        Returns a list of responses from all callbacks.
+        """
         if event.__class__ not in self.suscribers:
-            return
-        
+            return []
+        # copilot suggested to return a list of responses
+        responses = []
         for callback in self.suscribers[event.__class__]:   
-            callback(event)
+            response = callback(event)
+            if response is not None:
+                responses.append(response)
+
+        return responses
 
     def subscribe(self, event_class, callback):
+        """Subscribe a callback function to an event type."""
         if event_class not in self.suscribers:
             self.suscribers[event_class] = [callback]
             return
